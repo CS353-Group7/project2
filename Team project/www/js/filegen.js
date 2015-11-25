15ba3b1c4fe707ecbbf1f68f34556a3c740554ec
Toby Burns <toby8urns@gmail.com>
2015-11-25 12:15:33 +0000

    all levels

--- Team project/www/js/filegen.js
+++ Team project/www/js/filegen.js
@@ -1,12 +1,23 @@
-function file(){
+function file(int type){
+	if(type==1){
+		var pics = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+	}
+	else if(type==2){
+		var pics = ["happy","sad","angry","surprised","love","laughing","pensive","crying","sleepy","frustrated","scared","embarrassed","excited","smug","innocent","bored"];
+	}
+	else if(type==3){
+		var pics = [];
+	}
+	else{
+		var pics =["1","2","3","4","5","6","7","8","9"];
+	}
 	var counter = parseInt(document.getElementById("count").innerHTML);
 	if (counter < 8){
 		counter++;
 		document.getElementById("count").innerHTML=counter;
-		var emotes = ["happy","sad","angry","surprised","love","laughing","pensive","crying","sleepy","frustrated","scared","embarrassed","excited","smug","innocent","bored"];
 		
-		var value = Math.ceil(Math.random()*emotes.length)-1;
-		var str1 = emotes[value];
+		var value = Math.ceil(Math.random()*pics.length)-1;
+		var str1 = pics[value];
 		var find = document.getElementById("find");
 		find.innerHTML = str1;
 		
@@ -20,11 +31,11 @@ function file(){
 		
 		for(i=0;i<times.length;i++){
 			if (value > -1) {
-				emotes.splice(value, 1);
+				pics.splice(value, 1);
 			}
 			
-			value = Math.ceil(Math.random()*emotes.length)-1;
-			str1 = emotes[value];
+			value = Math.ceil(Math.random()*pics.length)-1;
+			str1 = pics[value];
 			
 			(document.getElementById(times[i])).src = "../img/emotions/" + str1 + ".png";
 			(document.getElementById(times[i])).onclick =  function (){alert('Whoops,you picked the wrong one. Try again!');return false;};
