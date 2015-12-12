e11fdc7009d18aa027f0a24d85b1f5d9bb5fb7d4
Toby Burns <toby8urns@gmail.com>
2015-12-12 14:33:26 +0000

    all levels are different now

--- Team project/www/js/filegen.js
+++ Team project/www/js/filegen.js
@@ -7,12 +7,15 @@ function file(type){
 		var folder = "alphabet";
 	}
 	else if(type==2){
-		var pics = ["happy","sad","angry","surprised","love","laughing","pensive","crying","sleepy","frustrated","scared","embarrassed","excited","smug","innocent","bored"];
+		if(levelnum==0) var pics = ["happy","sad","angry","surprised","sleepy","scared","pensive","innocent"];
+		else var pics = ["like they are in love","frustrated","like they are laughing","embarrassed","excited","smug","like they are crying","bored"];
 		var folder = "emotions";
-		
+
 	}
 	else if(type==3){
-		var pics = ["stop","greenlight","redlight","amberlight","roadclosed","roadworks","busstop","noentry","leftturn","rightturn","gostraight","nouturn","yield","gosign","greenman","redman","dualcarriageway","narrowroad","noovertaking","buslane","childrenahead","parking","noparking","dangerousbend"];
+		if(levelnum==0) var pics = ["stop","greenlight","redlight","amberlight","roadclosed","roadworks","busstop","noentry"];
+		else if(levelnum==1) var pics = ["leftturn","rightturn","gostraight","nouturn","yield","gosign","greenman","redman"];
+		else var pics = ["dualcarriageway","narrowroad","noovertaking","buslane","childrenahead","parking","noparking","dangerousbend"];
 		var folder = "roadsigns";
 	}
 	else{
@@ -20,11 +23,11 @@ function file(type){
 		var folder = "numbers";
 	}
 	var counter = parseInt(document.getElementById("count").innerHTML);
-	
+
 	if (counter < 8){
 		counter++;
 		document.getElementById("count").innerHTML=counter;
-		
+
 		var value = Math.ceil(Math.random()*pics.length)-1;
 		var str1 = pics[value];
 		var find = document.getElementById("find");
@@ -34,7 +37,7 @@ function file(type){
 		else{
 			find.innerHTML = str1;
 		}
-		
+
 		var times = ["one","two","three"];
 		var value1 = Math.ceil(Math.random()*times.length)-1;
 		(document.getElementById(times[value1])).src = "../img/"+folder+"/" + str1 + ".png";
@@ -42,29 +45,40 @@ function file(type){
 		if (value > -1) {
 			times.splice(value1, 1);
 		}
-		
+
 		for(i=0;i<times.length;i++){
 			if (value > -1) {
 				pics.splice(value, 1);
 			}
-			
+
 			value = Math.ceil(Math.random()*pics.length)-1;
 			str1 = pics[value];
-			
+
 			(document.getElementById(times[i])).src = "../img/"+folder+"/" + str1 + ".png";
 			(document.getElementById(times[i])).onclick =  function (){alert('Whoops,you picked the wrong one. Try again!');return false;};
 		}
 	}
 	else{
 		levelnum++;
-		if(levelnum>2) window.location = "welcome.html";
-		else window.location = "finished.html#"+levelnum+type;
+		if((type==1)|(type==3)){
+			if(levelnum>2) window.location = "alldone.html";
+			else window.location = "finished.html#"+levelnum+type;
+		}
+		else if(type==2){
+			if(levelnum>1) window.location = "alldone.html";
+			else window.location = "finished.html#"+levelnum+type;
+		}
+		else{
+			if(levelnum>0) window.location = "alldone.html";
+			else window.location = "finished.html#"+levelnum+type;
+		}
 	}
-	
+
 }
 
 function load(){
 	var type = parseInt((document.location.href).slice(-1));
+	var list = document.getElementsByClassName("btn btn-large btn-primary");
 	if(type==1){
 		document.getElementById("heading").innerHTML = "Alphabet";
 		document.getElementsByClassName("lead")[0].innerHTML = "Here we will play different levels that will test your knowledge on the alphabet.<br> First lets pick a level!";
@@ -72,6 +86,7 @@ function load(){
 	else if(type==2){
 		document.getElementById("heading").innerHTML = "Emotions";
 		document.getElementsByClassName("lead")[0].innerHTML = "Here we will play different levels that will test your knowledge on emotions.<br> First lets pick a level!";
+		list[2].parentElement.removeChild(list[2]);
 	}
 	else if(type==3){
 		document.getElementById("heading").innerHTML = "Road Signs";
@@ -80,8 +95,10 @@ function load(){
 	else{
 		document.getElementById("heading").innerHTML = "Numbers";
 		document.getElementsByClassName("lead")[0].innerHTML = "Here we will play different levels that will test your knowledge on the numbers.<br> First lets pick a level!";
+		list[2].parentElement.removeChild(list[2]);
+		list[1].parentElement.removeChild(list[1]);
 	}
-	var list = document.getElementsByClassName("btn btn-large btn-primary");
+	list = document.getElementsByClassName("btn btn-large btn-primary");
 	for(i = 0;i<list.length;i++){
 		list[i].href = "level.html#" + i + type;
 	}
@@ -115,4 +132,20 @@ function nextlevel(){
 	document.getElementById("which").innerHTML="Well Done! You Finished Level " + levelnum + "!";
 	levelnum++;
 	document.getElementById("levelnum").innerHTML="Continue to Level " + levelnum;
+}
+
+function alldone(){
+	var type = parseInt((document.location.href).slice(-1));
+	if(type==1){
+		document.getElementById("which").innerHTML="Well done! You finished the alphabet game!";
+	}
+	else if(type==2){
+		document.getElementById("which").innerHTML="Well done! You finished the emotions game!";
+	}
+	else if(type==3){
+		document.getElementById("which").innerHTML="Well done! You finished the road signs game!";
+	}
+	else{
+		document.getElementById("which").innerHTML="Well done! You finished the numbers game!";
+	}
 }
